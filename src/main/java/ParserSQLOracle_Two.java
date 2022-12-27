101a3c897d6c47e31e7c30ac5e4afc46ace3e199
Viktar Maroz <vmarozmail@gmail.com>
2022-12-27 18:49:08 +0100

    Added some canges to the existing files and added the thirth class

--- src/main/java/ParserSQLOracle_Two.java
+++ src/main/java/ParserSQLOracle_Two.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
-public class ParserSQLOracleOne {
+public class ParserSQLOracle_Two {
     public static void main(String[] args){
         Scanner parser = new Scanner(System.in);
         System.out.println("Hello! Enter your copy of unquoted data:");
 
         String parsed = parser.next();
+        while (parser.hasNext()){
+        System.out.println();
         String a1 = "'" + parsed + "', " +"'(NULL)', '(NULL)', ";
         parsed = parser.next();
         String a2 = "'" + parsed + "', ";
@@ -35,25 +37,10 @@ public class ParserSQLOracleOne {
         String a14 = "'" + parsed + "'";
 
 
-        System.out.print(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12+a13+a14);
+        System.out.print(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12+a13+a14);}
 
 
 //data example: 7641117a-cbe9-464f-920a-db896c9279d8	1	ddd1e5d7-093b-4a6b-b981-00ebb5ee59aa	92472f78-2735-48af-8859-0048a30a934c	01-NOV-22	02-NOV-22	XLSX	02f95491-3993-4430-90f7-0fb57530723f	28-NOV-22 10.38.38.736000000	TC011-B-USD	BCB-248_Direct_Participants_Table-A	TESTUSR1	0
 
     }
 }
-/*
-        Scanner scanner = new Scanner(System.in);
-        String initial = null;
-        String quoted = null;
-        do {
-            initial = scanner.nextLine();
-            String[] words = initial.split(" ");
-            for (int i = 0; i < words.length - 1; i++) {
-                words[i] = "'" + words[i] + "'";
-            }
-            quoted = String.join(", ", words);
-            System.out.println(quoted);
-        } while (!initial.equals("0"));
- */
-
