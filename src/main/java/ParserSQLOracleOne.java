ced14e2c1390babaae3eb7886b7f084e35cdfe26
Viktar Maroz <vmarozmail@gmail.com>
2022-12-27 14:39:16 +0100

    Initial commit

---
+++ src/main/java/ParserSQLOracleOne.java
@@ -0,0 +1,59 @@
+import java.util.Scanner;
+
+public class ParserSQLOracleOne {
+    public static void main(String[] args){
+        Scanner parser = new Scanner(System.in);
+        System.out.println("Hello! Enter your copy of unquoted data:");
+
+        String parsed = parser.next();
+        String a1 = "'" + parsed + "', " +"'(NULL)', '(NULL)', ";
+        parsed = parser.next();
+        String a2 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a3 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a4 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a5 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a6 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a7 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a8 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a9 = "'" + parsed + " ";
+        parsed = parser.next();
+        String a10 = parsed + "', ";
+        parsed = parser.next();
+        String a11 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a12 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a13 = "'" + parsed + "', ";
+        parsed = parser.next();
+        String a14 = "'" + parsed + "'";
+
+
+        System.out.print(a1+a2+a3+a4+a5+a6+a7+a8+a9+a10+a11+a12+a13+a14);
+
+
+//data example: 7641117a-cbe9-464f-920a-db896c9279d8	1	ddd1e5d7-093b-4a6b-b981-00ebb5ee59aa	92472f78-2735-48af-8859-0048a30a934c	01-NOV-22	02-NOV-22	XLSX	02f95491-3993-4430-90f7-0fb57530723f	28-NOV-22 10.38.38.736000000	TC011-B-USD	BCB-248_Direct_Participants_Table-A	TESTUSR1	0
+
+    }
+}
+/*
+        Scanner scanner = new Scanner(System.in);
+        String initial = null;
+        String quoted = null;
+        do {
+            initial = scanner.nextLine();
+            String[] words = initial.split(" ");
+            for (int i = 0; i < words.length - 1; i++) {
+                words[i] = "'" + words[i] + "'";
+            }
+            quoted = String.join(", ", words);
+            System.out.println(quoted);
+        } while (!initial.equals("0"));
+ */
+
